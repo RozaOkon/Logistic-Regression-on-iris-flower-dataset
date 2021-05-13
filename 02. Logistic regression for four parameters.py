7431ca65ee8b7b36c1600a4ed80b9a733b760c18
Róża Okoń <82170715+RozaOkon@users.noreply.github.com>
2021-05-13 20:56:31 +0200

    Update 02. Logistic regression for four parameters.py

--- 02. Logistic regression for four parameters.py
+++ 02. Logistic regression for four parameters.py
@@ -8,11 +8,6 @@ iris = datasets.load_iris()
 X = iris["data"]
 y = iris["target"]
 
-plt.hist(X[y==0, :])
-plt.hist(X[y==1, :])
-plt.hist(X[y==2, :])
-plt.show()
-
 log_reg = LogisticRegression(random_state=42)
 clf = log_reg.fit(X, y)
 X_new = np.linspace(0, 8, 1000).reshape(-1, 4)
@@ -24,6 +19,7 @@ plt.plot(X_new, y_proba_new[:, 0], "g-", linewidth=2, label="Setosa")
 plt.plot(X_new, y_proba_new[:, 1], "r-", linewidth=2, label="Versicolor")
 plt.plot(X_new, y_proba_new[:, 2], "y-", linewidth=2, label="Virginica")
 plt.legend()
+plt.title('Probability distribution for genetared points')
 plt.show()
 
 plt.clf()
@@ -31,7 +27,5 @@ plt.plot(X, y_proba[:, 0], "g-", linewidth=2, label="Setosa")
 plt.plot(X, y_proba[:, 1], "r-", linewidth=2, label="Versicolor")
 plt.plot(X, y_proba[:, 2], "y-", linewidth=2, label="Virginica")
 plt.legend()
+plt.title('Probability distribution for measured points')
 plt.show()
-#Dostajemy po jednym wykresie, ponieważ dopasowana krzywa jest superpozycją wszystkich danych
-#Na osi X mamy superpozycję wszystkich parametrów
-#Wagi z jakimi wchodzą konkretne parametry do tej superpozycji są szukane właśnie przez szukanie wzoru funkcji, czyli machine learning
